c0e07da4e5081ed63bcb9fcf714451e278187e79
Mariam <mariam.elgamal@nyu.edu>
2020-10-26 12:07:47 +0400

    data transfer commands

---
+++ data_ingest/data_transfer.sh
@@ -0,0 +1,13 @@
+hdfs dfs -put profiling.sql /user/netid/
+hdfs dfs -put analytic.sql /user/netid
+
+hdfs dfs -put broadband2017.csv /user/netid
+hdfs dfs -put broadband2016.csv /user/netid
+hdfs dfs -put broadband2015.csv /user/netid
+
+hdfs dfs -put combine17.txt /user/netid
+hdfs dfs -put combine16.txt /user/netid
+hdfs dfs -put combine15.txt /user/netid
+
+hdfs dfs -put ruralurban.tsv /user/netid/ruralurban
+hdfs dfs -put seattle_speed.tsv /user/netid/seattle
